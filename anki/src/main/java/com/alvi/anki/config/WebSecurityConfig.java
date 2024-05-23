3d17e58d39fd5348656231a5c805e427ec01d124
aleh.vilchynski <vilchinskioleg@gmail.com>
2024-05-23 21:41:49 +0200

    Refactored classes

--- anki/src/main/java/com/alvi/anki/config/WebSecurityConfig.java
+++ anki/src/main/java/com/alvi/anki/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.alvi.anki.config;
 
+import com.alvi.anki.filter.PopulateAuthoritiesFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
--- anki/src/main/java/com/alvi/anki/filter/PopulateAuthoritiesFilter.java
+++ anki/src/main/java/com/alvi/anki/filter/PopulateAuthoritiesFilter.java
@@ -1,4 +1,4 @@
-package com.alvi.anki.config;
+package com.alvi.anki.filter;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
